c60eb4cab563e3b2a62db5262c3069716770b446
Amr Mohamed Fathallah <amrmfathallah11@gmail.com>
2025-05-10 06:59:54 +0300

    update debug files

--- BackEnd/FCIH.ArtGallery/FCIH.ArtGallery.Core.Application.Abstraction/obj/Debug/net8.0/FCIH.ArtGallery.Core.Application.Abstraction.AssemblyInfo.cs
+++ BackEnd/FCIH.ArtGallery/FCIH.ArtGallery.Core.Application.Abstraction/obj/Debug/net8.0/FCIH.ArtGallery.Core.Application.Abstraction.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FCIH.ArtGallery.Core.Application.Abstraction")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41a86c993a646276cdb5afa721f1aefc5339aa7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b0a1fc22134dea5a6841c439cde259168bd8702")]
 [assembly: System.Reflection.AssemblyProductAttribute("FCIH.ArtGallery.Core.Application.Abstraction")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FCIH.ArtGallery.Core.Application.Abstraction")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
